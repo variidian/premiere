3656f7d128fe2d39daea258a74db434ff3ebeff7
turdle <shuruiguo1@gmail.com>
2026-04-22 19:30:31 +1000

    guh

--- src/scripts/auth.js
+++ src/scripts/auth.js
@@ -2,6 +2,6 @@ const API = import.meta.env.PUBLIC_API_URL || 'http://localhost:3000';
 export async function getUser() {
     const token = localStorage.getItem('token');
     if (!token) return null;
-    const res = await fetch(`${API}/api/auth/me?token=${token}`);
+    const res = await fetch(`${API}/auth/me?token=${token}`);
     return await res.json();
 }
